8318256616e6f48122c050a43ebb7916f8d40a03
promabhatt1 <promabhattacharjee1proff@gmail.com>
2024-02-20 23:11:08 +0600

    added js files

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+// BUTTON A1 COLOR CHANGE 
+const buttonA1 = document.getElementById('a-one');
+buttonA1.addEventListener('click',function(){
+    buttonA1.style.backgroundColor = 'green';
+});
+// BUTTON A2 COLOR CHANGE 
+const buttonA2 = document.getElementById('a-two');
+buttonA2.addEventListener('click',function(){
+    buttonA2.style.backgroundColor = 'green';
+});
+// BUTTON A3 COLOR CHANGE 
+const buttonA3 = document.getElementById('a-three');
+buttonA3.addEventListener('click',function(){
+    buttonA3.style.backgroundColor = 'green';
+});
+// BUTTON A4 COLOR CHANGE 
+const buttonA4 = document.getElementById('a-four');
+buttonA4.addEventListener('click',function(){
+    buttonA4.style.backgroundColor = 'green';
+});
+// BUTTON B1 COLOR CHANGE 
+const buttonB1 = document.getElementById('b-one');
+buttonB1.addEventListener('click',function(){
+    buttonB1.style.backgroundColor = 'green';
+});
+// BUTTON B2 COLOR CHANGE 
+const buttonB2 = document.getElementById('b-two');
+buttonB2.addEventListener('click',function(){
+    buttonB2.style.backgroundColor = 'green';
+});
+// BUTTON B3 COLOR CHANGE 
+const buttonB3 = document.getElementById('b-three');
+buttonB3.addEventListener('click',function(){
+    buttonB3.style.backgroundColor = 'green';
+});
+// BUTTON B4 COLOR CHANGE 
+const buttonB4 = document.getElementById('b-four');
+buttonB4.addEventListener('click',function(){
+    buttonB4.style.backgroundColor = 'green';
+});
+
+
